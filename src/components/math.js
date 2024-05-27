3847611670535df141446abdd422363dce98d7d7
Erico Almeida <erico.silva@banqi.com>
2024-05-27 02:23:42 -0400

    feat: add sum operation

---
+++ src/components/math.js
@@ -0,0 +1,7 @@
+const add = (valueOne, valueTwo) => {
+    return valueOne + valueTwo;
+}
+
+module.exports = {
+    add
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const { add } = require("./components/math.js")
+
+const sumResult = add(3,5);
+
+console.log(sumResult)
